e4d3e7601f175400433209948025656138cfaf87
Benyair Orellana <ben.orellana@gmail.com>
2020-04-20 22:14:41 -0400

    switched to heroku backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  // serverURL: 'https://torshavn-api.herokuapp.com/api/',
-  serverURL: 'http://ec2-3-83-8-189.compute-1.amazonaws.com/api/',
+  // serverURL: 'https://torshavn-api.herokuapp.com/api/', //Java
+  // serverURL: 'http://ec2-3-83-8-189.compute-1.amazonaws.com/api/', //aws python
+  serverURL: 'https://torshavn-api-python.herokuapp.com/api/',
   googleMapsApiKey: 'AIzaSyCEroEM0m4QzyhQcqeK2GvCc0qDlCIzcAA',
   s3Url: 'https://torhavn-maps.s3.us-east-2.amazonaws.com/images/'
 };
