2ff9a42eed722da92a30ff309e7875c47636bd8a
Benyair Orellana <ben.orellana@gmail.com>
2020-05-01 13:25:35 -0400

    interceptor

--- src/app/interceptors/request.interceptor.ts
+++ src/app/interceptors/request.interceptor.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class RequestInterceptor implements HttpInterceptor {
 
-    constructor() {}
+    constructor(private authService: AuthService) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        request = request.clone({
-        });
-
-        return next.handle(request);
+      // We retrieve the token, if any
+        const token = this.authService.token;
+        let newHeaders = request.headers;
+        if (token) {
+            // If we have a token, we append it to our new headers
+            newHeaders = newHeaders.append('Authorization', 'JWT ' + token);
+        }
+        // Finally we have to clone our request with our new headers
+        // This is required because HttpRequests are immutable
+        const authReq = request.clone({headers: newHeaders});
+        // Then we return an Observable that will run the request
+        // or pass it to the next interceptor if any
+        return next.handle(authReq);
     }
 }
